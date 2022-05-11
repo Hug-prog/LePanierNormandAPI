d77e3eac956927557f50254b9884565f812b8ffb
Dylan Aumond <dylanaumond@gmail.com>
2022-05-11 22:20:10 +0200

    add/remove product to wishlist

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use App\Entity\Product;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -45,6 +46,10 @@ class UserController extends AbstractController
     public function getCurrentUser(): Response
     {
         $user=$this->getUser();
+        $likedProducts = [];
+        foreach($user->getLikedProducts() as $likedProduct){
+            $likedProducts[]=$likedProduct->getId();
+        }
         $queryData = [
             'firstname' => $user->getFirstname(),
             'lastname' => $user->getLastname(),
@@ -53,7 +58,53 @@ class UserController extends AbstractController
             'street' => $user->getStreet(),
             'postCode' => $user->getPostCode(),
             'city' => $user->getCity(),
+            'likedProducts' => $likedProducts,
         ];
         return $this->json($queryData);
+    }
+
+    /**
+     * @Route("/user/likedproducts", name="get_user_liked_products", methods={"GET"})
+     */
+    public function getUserLikedProducts(): Response
+    {
+        $user=$this->getUser();
+        $likedProducts=[];
+        foreach($user->getLikedProducts() as $product){
+            $likedProducts[]=[
+                'id' => $product->getId(),
+               'libelle' => $product->getlibelle(),
+               'price' => $product->getPrice(),
+               'stock'=> $product->getStock(),
+               'description' => $product->getDescription(),
+               'seller' => $product->getProductSel()->getId(),
+               'images' => $product->getImages()
+            ];
+        }
+        return $this->json($likedProducts);
+    }
+    /**
+     * @Route("/user/like/product/{id}", methods={"patch"})
+     */
+    public function likeProduct(ManagerRegistry $doctrine,int $id){
+        $entityManager = $doctrine->getManager();
+        $repository = $doctrine->getRepository(Product::class);
+        $product = $repository->find($id);
+        $user = $this->getUser()->addLikedProduct($product);
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return $this->json('ajouté à la wishlist');
+    }
+    /**
+     * @Route("/user/unlike/product/{id}", methods={"patch"})
+     */
+    public function unlikeProduct(ManagerRegistry $doctrine,int $id){
+        $entityManager = $doctrine->getManager();
+        $repository = $doctrine->getRepository(Product::class);
+        $product = $repository->find($id);
+        $user = $this->getUser()->removeLikedProduct($product);
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return $this->json('retiré à la wishlist');
     }
 }
